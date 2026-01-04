9a0823703050e744724b6fe7e3dad07ef1fdf1b8
Ілля Цап <its.experimental.version.0@outlook.com>
2026-01-04 21:09:59 +0200

    feat: add "TrackConverter" project

---
+++ TFT10Player.TrackConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+Directory.CreateDirectory(Path.Combine("Tracks", "All"));
+Directory.EnumerateFiles(Path.Combine("Wav", "All")).AsParallel().Select(path =>
+{
+    Console.WriteLine($"Converting: {path}");
+    string str = $"{Path.GetFileNameWithoutExtension(path)}.pcm";
+    FileStream input = File.OpenRead(path);
+    input.Position = 44;
+    FileStream output = File.OpenWrite(Path.Combine("Tracks", "All", str));
+    Span<byte> span = stackalloc byte[2];
+    while(input.Position < input.Length)
+    {
+        input.ReadExactly(span);
+        float value = MemoryMarshal.Read<short>(span) / (float)short.MaxValue;
+        output.Write(BitConverter.GetBytes(value));
+    }
+    Console.WriteLine($"Done: {str}");
+    return "";
+}).ToArray();
