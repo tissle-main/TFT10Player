b96f4ed2225ac2216e5e124f2d1d6eca7ed1fe8e
Ілля Цап <its.experimental.version.0@outlook.com>
2026-01-17 16:16:45 +0200

    fix: change sample rate of wav tracks from 44100 to 48000

--- TFT10Player.TrackConverter/Program.cs
+++ TFT10Player.TrackConverter/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System.Runtime.InteropServices;
 
-Directory.CreateDirectory(Path.Combine("Tracks", "All"));
-Directory.EnumerateFiles(Path.Combine("Wav", "All")).AsParallel().Select(path =>
+Directory.CreateDirectory("TFT10Player");
+Directory.EnumerateFiles("Wav").AsParallel().Select(path =>
 {
     Console.WriteLine($"Converting: {path}");
     string str = $"{Path.GetFileNameWithoutExtension(path)}.pcm";
     FileStream input = File.OpenRead(path);
     input.Position = 44;
-    FileStream output = File.OpenWrite(Path.Combine("Tracks", "All", str));
+    FileStream output = File.OpenWrite(Path.Combine("TFT10Player", str));
     Span<byte> span = stackalloc byte[2];
     while(input.Position < input.Length)
     {
